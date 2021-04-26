44afd06bd48dcc13ef1e93685f3202513d6629b0
adecool <tobiadebiyi17@gmail.com>
2021-04-27 00:31:25 +0100

    commit

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from decouple import config
 from pathlib import Path
 import os
+import django_heroku
+django_heroku.settings(locals())
+TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
