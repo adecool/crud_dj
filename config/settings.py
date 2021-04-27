f15d1a091aa70bf16aeab3aad695eef6e457b75b
adecool <39190386+adecool@users.noreply.github.com>
2021-04-27 16:47:30 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import django_heroku
 from pathlib import Path
-from django-decouple import config
+from python-decouple import config
 
 
 
