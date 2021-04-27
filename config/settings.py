23191e3b216752598d4184fb11b5e3f0e57cf81b
adecool <tobiadebiyi17@gmail.com>
2021-04-27 13:58:08 +0100

    commit

--- config/settings.py
+++ config/settings.py
@@ -11,17 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-from os import environ
 from pathlib import Path
+from os import decouple
+from decouple import config
 
-
-from os import environ
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-# reading .env file
-environ.Env.read_env()
 
 
 
@@ -42,11 +35,11 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # False if not in os.environ
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
